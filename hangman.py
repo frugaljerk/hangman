cbc73c7419a47dd49ebabf4e4c84fd90aee6b467
frugaljerk <73091165+frugaljerk@users.noreply.github.com>
2021-07-01 00:30:17 +0800

    Add files via upload
    Hangman.py forked from Kylie Ying's python tutorial.
    It has added feature of hangman visual effect.

--- hangman.py
+++ hangman.py
@@ -1,72 +1,53 @@
-"""
-Hangman implementation by Kylie Ying
-
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-"""
-
 import random
-from words import words
-from hangman_visual import lives_visual_dict
+from words import words, man
 import string
 
-
-def get_valid_word(words):
-    word = random.choice(words)  # randomly chooses something from the list
+def validate_word(words):
+    word = random.choice(words) #randomly choose something from the list
     while '-' in word or ' ' in word:
         word = random.choice(words)
-
     return word.upper()
 
-
 def hangman():
-    word = get_valid_word(words)
-    word_letters = set(word)  # letters in the word
-    alphabet = set(string.ascii_uppercase)
-    used_letters = set()  # what the user has guessed
+    #Keep track on user inputs
+    word = validate_word(words)
 
-    lives = 7
+    letters_word = set(word) #letters in the word
+    letters_used = set() #user's guesses
+    alphabets = set(string.ascii_uppercase)
+    lives = 10
+    #adding user input to the sets
+    while len(letters_word) > 0 and lives > 0 :
+        #letter used
+        # ' '.join(['a', 'b','cd']) ---> a b cd
+        print(f'You have {lives} remaing and you have used these letters:' + ' '.join(letters_used))
 
-    # getting user input
-    while len(word_letters) > 0 and lives > 0:
-        # letters used
-        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+        #what current word is (ie W-RD)
+        word_list = [letters if letters in letters_used else '-' for letters in word ]
+        print("Current word: ", ' '.join(word_list))
+        print(man[10-lives])
 
-        # what current word is (ie W - R D)
-        word_list = [letter if letter in used_letters else '-' for letter in word]
-        print(lives_visual_dict[lives])
-        print('Current word: ', ' '.join(word_list))
-
-        user_letter = input('Guess a letter: ').upper()
-        if user_letter in alphabet - used_letters:
-            used_letters.add(user_letter)
-            if user_letter in word_letters:
-                word_letters.remove(user_letter)
-                print('')
-
+        user_input = input("Please enter a letter:").upper()
+        if user_input in alphabets - letters_used:
+            letters_used.add(user_input)
+            if user_input in letters_word:
+                letters_word.remove(user_input)
             else:
-                lives = lives - 1  # takes away a life if wrong
-                print('\nYour letter,', user_letter, 'is not in the word.')
+                lives = lives - 1 #minus one life for incorrect guess
+                print('Letter is not in the word')
 
-        elif user_letter in used_letters:
-            print('\nYou have already used that letter. Guess another letter.')
-
+        elif user_input in letters_used:
+            print('You have used this letter. Please try again:')
         else:
-            print('\nThat is not a valid letter.')
+            print('Invalid character. Please try again:')
 
-    # gets here when len(word_letters) == 0 OR when lives == 0
     if lives == 0:
-        print(lives_visual_dict[lives])
-        print('You died, sorry. The word was', word)
+        print(man[10])
+        print(f'Sorry you died. The word was {word}')
     else:
-        print('YAY! You guessed the word', word, '!!')
+        print(man[10-lives])
+        print(f'You WON!! The word wsa {word}')
 
 
-if __name__ == '__main__':
-    hangman()
+
+hangman()
